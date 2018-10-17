a3ed4f17c761a61fb000d0b5dcaf22601c4e490a
Eric Coleman <eric@epiccoleman.com>
2018-10-17 14:53:48 -0400

    update main to use Vec3 to represent color
    which barely makes any sense, honestly, and the code in the book is...
    weird, at best. but what the hell.

--- graphics/raytracing/main.cpp
+++ graphics/raytracing/main.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
+#include "Vec3.h"
 
 int main(){
-    int nx = 200;
-    int ny = 100;
+  int nx = 200;
+  int ny = 100;
 
-    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+  std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-    for(int j = ny - 1; j >= 0; j--){
-        for(int i = 0; i < nx; i++){
-            float r = float(i) / nx;
-            float g = float(j) / ny;
-            float b = 0.2;
+  for(int j = ny - 1; j >= 0; j--){
+    for(int i = 0; i < nx; i++){
+      // Vec3 color(float(i) / float(nx), float(j) / float(ny), 0.2);
+      // i like this better because its purty
+      Vec3 color(0.2, float(i) / float(nx), float(j) / float(ny)); 
 
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
-            std::cout << ir << " " <<  ig << " " << ib << "\n";
-        }
+      int ir = int(255.99 * color.r());
+      int ig = int(255.99 * color.g());
+      int ib = int(255.99 * color.b());
+      std::cout << ir << " " <<  ig << " " << ib << "\n";
     }
+  }
 }
