fa7672b821152848ae431cb0f3d2ddebef3d05ef
Eric Coleman <eric@epiccoleman.com>
2018-11-30 14:24:39 -0500

    optimize for single precision calc and time cuda exec

--- graphics/mandelbrot/main.cu
+++ graphics/mandelbrot/main.cu
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <iostream>
 #include <cmath>
 #include <vector>
+#include <time.h>
 
 #define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
 void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line) {
@@ -15,17 +15,16 @@ void check_cuda(cudaError_t result, char const *const func, const char *const fi
 }
 
 __global__ void color_pixel(int* pixels, int x_size, int y_size, int max_iterations){
-
   int i = threadIdx.x + blockIdx.x * blockDim.x;
   int j = threadIdx.y + blockIdx.y * blockDim.y;
-  if((i >= x_size) || (j >= y_size)) return;
+  if((i >= x_size) || (j >= y_size)) return;  // make sure we're actually operating on the array
 
-  double c_re = (i - x_size/2.0)*4.0/x_size;
-  double c_im = (j - y_size/2.0)*4.0/x_size;
-  double x = 0, y = 0;
+  float c_re = (i - x_size/2.0f)*4.0f/x_size;
+  float c_im = (j - y_size/2.0f)*4.0f/x_size;
+  float x = 0, y = 0;
   int iteration = 0;
   while (x*x+y*y <= 4 && iteration < max_iterations) {
-    double x_new = x*x - y*y + c_re;
+    float x_new = x*x - y*y + c_re;
     y = 2*x*y + c_im;
     x = x_new;
     iteration++;
@@ -41,30 +40,36 @@ __global__ void color_pixel(int* pixels, int x_size, int y_size, int max_iterati
 }
 
 int main(int argc, char *argv[]){
-  int nx = 1200;
+  int nx = 1000;
   int max = 1000;
-  int tx = 8;
-  int ty = 8;
+  int tx = 8;  // number of thread blocks 
+  int ty = 8;  // number of threads in a block
 
   if(argc > 1){
     nx = atoi(argv[1]);
   }
 
-  int ny = nx;
+  int ny = nx;   // for now, grid must be square, so ny = nx
 
   //allocate color values array
   int num_pixels = nx * ny;
   size_t pixels_size = num_pixels * sizeof(int);
 
   int *pixels;
-  checkCudaErrors(cudaMallocManaged((void **)&pixels, pixels_size));
+  checkCudaErrors(cudaMallocManaged((void **)&pixels, pixels_size));  // allocate shared memory 
 
-  dim3 blocks(nx/tx+1,ny/ty+1);
-  dim3 threads(tx,ty);
+  clock_t start, stop;
+  start = clock();
+
+  dim3 blocks(nx/tx+1,ny/ty+1); // 1221 blocks 
+  dim3 threads(tx,ty);  // total of tx * ty (64) threads in a block
   color_pixel<<<blocks, threads>>>(pixels, nx, ny, max);
-
   checkCudaErrors(cudaGetLastError());
   checkCudaErrors(cudaDeviceSynchronize());
+
+  stop = clock();
+  double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
+  std::cerr << "took " << timer_seconds << " seconds.\n";
 
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
   for(int x = 0; x < nx; x++){
